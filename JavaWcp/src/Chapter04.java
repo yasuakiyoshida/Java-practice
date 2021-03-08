349a8ce6797732b6342ab067fbc7c07e831660a9
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-03-08 14:58:48 +0900

    chapter04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String str1 = "yasuaki yoshida";
+		System.out.println(str1);
+		
+		str1 = "Java practice!";
+		System.out.println(str1);
+		
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
