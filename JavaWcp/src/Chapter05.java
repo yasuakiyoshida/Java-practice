97388589ace1c54140cbe76129ef6352bfc4208c
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-03-08 16:05:55 +0900

    chapter05

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,22 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		long long1 = 123456789L;
+		double double1 = 12345.6789;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float float1 = 123.456789F;
+		Integer integer1 = 12345678;
+		Character character1 = 'あ';
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(character1);
+		
+		int[] array = new int[] { 10, 20, 30, 40, 50 };
+		System.out.println(array[3]);
+	}
+}
