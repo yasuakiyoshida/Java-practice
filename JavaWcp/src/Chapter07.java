b37244ac04d2cea464a281ce295475df685845de
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-03-11 11:06:40 +0900

    chapter07

---
+++ JavaWcp/src/Chapter07.java
@@ -0,0 +1,32 @@
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		// 問題1
+		int distance = 10;
+		if (distance <= 5) {
+			System.out.println("とても近いです");
+		} else if (distance <= 10) {
+			System.out.println("近いです");
+		} else if (distance <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+		
+		// 問題2
+		String color = "red";
+		switch (color) {
+		case "red":
+			System.out.println("赤信号です");
+			break;
+		case "yellow":
+			System.out.println("黄色信号です");
+			break;
+		case "bule":
+			System.out.println("青信号です");
+			break;
+		default:
+			System.out.println("信号の色ではありません");
+		}
+	}
+}
